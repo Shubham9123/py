e6b88c583de2b96097523abd5021524721bacbab
Shubham9123 <shubhamsehgal100@gmail.com>
2020-04-14 02:05:09 +0530

    deleted main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-class Person:def
-__init__ (self, name, surname, age):
-self.name = name self.surname = surname self.age = age def greetings (self):
-print ("Hello", self.name, self.surname + ". Glad you are", self.age,
-       "years old.") p1 = Person ("Linus", "Torvalds", 52) p1.greetings ()
